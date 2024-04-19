c341381ef66a0c72ce82278df46a8f0a4d35a3b4
jossnock <144459459+jossnock@users.noreply.github.com>
2024-04-19 14:16:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 """
-It's up to you, intrepid warrior, to glue the parts together. 
 Write the function that parses the mileage number input, and returns:
-    a 2 if the number is "interesting" (see below)
-    a 1 if an interesting number occurs within the next two miles
-    a 0 if the number is not interesting
-
-Note: In Haskell, we use No, Almost and Yes instead of 0, 1 and 2.
+    True if the number is "interesting" (see below)
+    False if the number is not interesting
 
 "Interesting" Numbers
 Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
 
-- Any digit followed by all zeros: 100, 90000
-- Every digit is the same number: 1111
+-- Any digit followed by all zeros: 100, 90000
+-- Every digit is the same number: 1111
 - The digits are sequential, incementing†: 1234
 - The digits are sequential, decrementing‡: 4321
 - The digits are a palindrome: 1221 or 73837
@@ -19,19 +15,43 @@ Interesting numbers are 3-or-more digit numbers that meet one or more of the fol
 
 † For incrementing sequences, 0 should come after 9, and not before 1, as in 7890.
 ‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
+
+- The awesomePhrases array will always be provided, and will always be an array, but may be empty. (Not everyone thinks numbers spell funny words...)
 """
 
-def is_interesting(number):
+def is_interesting(number, interesting_numbers = []):
+    if number in interesting_numbers:
+        return True
     j = 0
-    for i in range(3):
+    try:
+        # Any digit followed by all zeros
         number_str = str(number)
-        if number_str[1:] == 0*(len(number_str)-1):
-            return 2 - j
+        if number_str[1:] == ("0")*(len(number_str)-1):
+            return True
+        # Every digit is the same number: 1111
+        if number_str == number_str[0]*len(number_str):
+            return True
+        
+        # The digits are sequential, incementing: 1234
+        
 
-        if j == 0:
-            j = 1
-        number += 1
-    return 0
+        # The digits are sequential, decrementing: 4321
+        # The digits are a palindrome: 1221 or 73837
+        # The digits match one of the values in the awesome_phrases array
+    except:
+        ValueError
+    return False
 
 
-print(is_interesting(1000000))
+assert(is_interesting(1000000) == True)
+assert(is_interesting(9000000000000000) == True)
+assert(is_interesting(200) == True)
+assert(is_interesting("fit") == False)
+assert(is_interesting(11111) == True)
+assert(is_interesting(5555) == True)
+assert(is_interesting(12312, [12312, 234987234, 131023.123]) == True)
+
+number = str(123456)
+a = 0
+for i in number:
+    a += 1
