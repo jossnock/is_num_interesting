d20ebd6ba9cdc4ca04c0ba3e10ff0f14c1ff8740
JossNockolds <19121867@hlnsc.ac.uk>
2024-04-20 22:12:47 +0100

    works fully!

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sturdy-broccoli
+
+## A function that takes an integer input and returns:
+ - True if the number is "interesting" (see below)
+ - False if the number is not interesting
+
+## "Interesting" Numbers:
+ - Any digit followed by all zeros: 100, 90000
+ - Every digit is the same number: 1111
+ - The digits are sequential, incementing†: 1234
+ - The digits are sequential, decrementing‡: 4321
+ - The digits are a palindrome: 1221 or 73837
+ - The digits match one of the values in the interesting_numbers array
+
+ † For incrementing sequences, 0 should come after 9, and not before 1, as in 7890.
+ ‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
--- main.py
+++ main.py
@@ -1,57 +1,112 @@
-"""
-Write the function that parses the mileage number input, and returns:
-    True if the number is "interesting" (see below)
-    False if the number is not interesting
+import math
 
-"Interesting" Numbers
-Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
+def digit_trunc(number):
+    """Returns the last digit of a number."""
+    return int(str(number)[-1])
 
--- Any digit followed by all zeros: 100, 90000
--- Every digit is the same number: 1111
-- The digits are sequential, incementing†: 1234
-- The digits are sequential, decrementing‡: 4321
-- The digits are a palindrome: 1221 or 73837
-- The digits match one of the values in the awesome_phrases array
+def is_num_interesting(number, interesting_numbers = []):
+    """
+    Parameters:
+     - number: int, 
+     - interesting_numbers: list, 
 
-† For incrementing sequences, 0 should come after 9, and not before 1, as in 7890.
-‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
-
-- The awesomePhrases array will always be provided, and will always be an array, but may be empty. (Not everyone thinks numbers spell funny words...)
-"""
-
-def is_interesting(number, interesting_numbers = []):
-    if number in interesting_numbers:
-        return True
-    j = 0
+    Returns:
+     - True if number is interesting
+     - False if number isn't interesting
+    
+    range(i, j) produces i, i+1, i+2, ..., j-1. 
+    start defaults to 0, and stop is omitted! range(4) produces 0, 1, 2, 3. 
+    These are exactly the valid indices for a list of 4 elements. 
+    step is given, it specifies the increment (or decrement).
+    """
     try:
-        # Any digit followed by all zeros
+        # normalising parameters:
+        int(number)
+        
+        if type(interesting_numbers) == type(12345) or type(interesting_numbers) == type("12345"): # if int or str
+            int(interesting_numbers)
+            interesting_numbers = set([interesting_numbers])
+        else:
+            set(interesting_numbers)
+        
         number_str = str(number)
-        if number_str[1:] == ("0")*(len(number_str)-1):
+
+        # Any digit followed by all zeros
+        if number_str[1:] == ("0") * (len(number_str) - 1):
             return True
+        
         # Every digit is the same number: 1111
-        if number_str == number_str[0]*len(number_str):
+        if number_str == number_str[0] * len(number_str):
             return True
         
         # The digits are sequential, incementing: 1234
+        is_asc = True
+        prev_i = 0 # placeholder value
+        for i in number_str:
+            if i != number_str[0]:
+                if digit_trunc(int(prev_i) + 1) != int(i):
+                    is_asc = False
+                    break
+            prev_i = i
+        if is_asc == True:
+            return True
         
-
         # The digits are sequential, decrementing: 4321
-        # The digits are a palindrome: 1221 or 73837
-        # The digits match one of the values in the awesome_phrases array
+        is_dec = True
+        prev_i = 0 # placeholder value
+        number_str_inverted = number_str[::-1]
+        for i in number_str_inverted:
+            if i != number_str_inverted[0]:
+                if digit_trunc(int(prev_i) + 1) != int(i):
+                    is_dec = False
+                    break
+            prev_i = i
+        if is_dec == True:
+            return True
+
+        # The digits are a palindrome: 73837
+        is_palin = True
+        for i in range(math.trunc(len(number_str) / 2) + 1): # only needs to check halfway through the list (rounded up)
+            if number_str[i] != number_str[-(i + 1)]:
+                is_palin = False
+                break
+        if is_palin == True:
+            return True
+
+        # The digits match one of the values in the interesting_numbers array
+        if number in interesting_numbers:
+            return True
+        
     except:
         ValueError
+
     return False
 
 
-assert(is_interesting(1000000) == True)
-assert(is_interesting(9000000000000000) == True)
-assert(is_interesting(200) == True)
-assert(is_interesting("fit") == False)
-assert(is_interesting(11111) == True)
-assert(is_interesting(5555) == True)
-assert(is_interesting(12312, [12312, 234987234, 131023.123]) == True)
+# asserts:
 
-number = str(123456)
-a = 0
-for i in number:
-    a += 1
+assert(digit_trunc(10) == 0)
+assert(digit_trunc(24356) == 6)
+
+assert(is_num_interesting(9000000000000000) == True)
+assert(is_num_interesting(200) == True)
+
+assert(is_num_interesting("quick") == False)
+assert(is_num_interesting("boop oop soup") == False)
+
+assert(is_num_interesting(11111) == True)
+assert(is_num_interesting(5555) == True)
+
+assert(is_num_interesting(12312, {12312, 234987234, 131023.123}) == True)
+assert(is_num_interesting("testing", {"testing"}) == False) # only accepts ints
+assert(is_num_interesting(314159, 314159) == True) # only accepts lists/arrays
+
+
+assert(is_num_interesting(45678901234567) == True)
+assert(is_num_interesting(89012) == True)
+
+assert(is_num_interesting(7654) == True)
+assert(is_num_interesting(21098) == True)
+
+assert(is_num_interesting(14641) == True)
+assert(is_num_interesting(1001) == True)
